58fb5affc4624f4a851e21f5c894fdfa863c55ce
outtouniverse <aakub1096@gmail.com>
2024-08-02 16:05:12 +0530

    enough

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: 'https://vibe-zz-re.vercel.app/',
+  //origin: 'https://vibe-zz-re.vercel.app/',''
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true, 
 }));
