bc03235e93f4e560d10a4ab070a62c5cb81426c2
outtouniverse <aakub1096@gmail.com>
2024-08-02 13:38:24 +0530

    tyin_server

--- server/server.js
+++ server/server.js
@@ -17,7 +17,6 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 
-// CORS configuration
 app.use(cors({
   origin: ['https://vibe-zz.vercel.app', 'http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
