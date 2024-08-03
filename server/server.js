9472b945a6d525de266e3d3c37953ec7c867d9da
outtouniverse <aakub1096@gmail.com>
2024-08-03 11:07:05 +0530

    New_cors_commit

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: 'https://vibe-zz-re.vercel.app', 
+  origin: ['https://vibe-zz-re.vercel.app',"http://localhost:3000/" ],
   credentials: true, 
 }));
 
