033aff0aaaf76ed9a46a55eaa915d154246e9ae2
outtouniverse <aakub1096@gmail.com>
2024-08-04 17:46:50 +0530

    login_cred

--- client/vibezz/src/Components/Login.jsx
+++ client/vibezz/src/Components/Login.jsx
@@ -66,6 +66,7 @@ export default function Login() {
         bio: data.bio,
         profilepic: data.profilepic,
       }));
+  
       
       
       setUser({
--- server/utils/helpers/token_cookie.js
+++ server/utils/helpers/token_cookie.js
@@ -1,19 +1,16 @@
 import jwt from 'jsonwebtoken';
 
-const token_cookie=(userId,res)=>{
-    const token=jwt.sign({userId},process.env.JWT_SECRET,{
-        expiresIn:'15d',
-    })
-    res.cookie("jwt",token,{
-        httpOnly:true,
-        maxAge:15*24*60*60*1000,
-        sameSite:"strict",
-        secure: process.env.NODE_ENV === 'production'
-
-    })
-    return token;
-
-
-}
+const token_cookie = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: '15d',
+  });
+  res.cookie('jwt', token, {
+    httpOnly: true,
+    maxAge: 15 * 24 * 60 * 60 * 1000,
+     sameSite: 'lax',
+    secure: process.env.NODE_ENV === 'production',
+  });
+  return token;
+};
 
 export default token_cookie;
